6b8b560806fabbb7e9cb8c34e04f58a03af84a0d
kalz2q <katsu95i@gmail.com>
2022-10-17 10:04:02 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,39 @@ import './App.css';
 const { useState } = React;
 // next button で次を random に表示する
 
-
 function App() {
+  const [stoppedTime, setStoppedTime] = useState(0);
+  const [time, setTime] = useState(0);
+  const [lastStartedAt, setLastStartedAt] = useState(0);
+  const [currentChoice, setCurrentChoice] = useState(namesList[Math.floor(Math.random() * namesList.length)])
+
+  const calcTime = () => {
+    return lastStartedAt === 0
+      ? stoppedTime
+      : stoppedTime + new Date().getTime() - lastStartedAt;
+  };
+
+  const timer = () => {
+    if (lastStartedAt === 0) {
+      setLastStartedAt(new Date().getTime());
+    } else {
+      setStoppedTime(calcTime());
+      setLastStartedAt(0);
+    }
+    return;
+  };
+
+  requestAnimationFrame(() => setTime(calcTime()));
 
   return (
-    <div className="RandomPicker__choice">
-      <span className="RandomPicker__choiceItem">{namesList[2]}</span>
+    <div className="App">
+      <span>{currentChoice}</span>
+      <p>timer (ms): {time}</p>
+      <p>
+        <button onClick={() => timer()}>
+          {lastStartedAt === 0 ? "start" : "stop"}
+        </button>
+      </p>
     </div>
   );
 }
